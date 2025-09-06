589ebe10674565a0747cac58f437a2cbca053670
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-06 01:26:22 -0300

    fix: Infos index, metatags, sitemap e robots

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -50,10 +50,10 @@ import { FileText } from "lucide-react";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Dra. Giovanna Maciel",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
-  domain: "A_Definir", // Sem "www"
+  domain: "giovannamacieladv.com.br", // Sem "www"
   phone: {
     ddd: "A_Definir",
     firstPart: "A_Definir", // Apenas os 5 primeiros números (incluindo o 9)
